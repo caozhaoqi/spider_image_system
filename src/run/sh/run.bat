c1358b1f197d6b52bc56dff2e9eab6c48f3eb3b8
caozhaoqi <1150118968@qq.com>
2024-02-26 11:28:34 +0800

    run script update

--- src/run/sh/run.bat
+++ src/run/sh/run.bat
@@ -1,2 +1,24 @@
-python ../ui_main.py
+@echo off
+setlocal enabledelayedexpansion
+
+REM 指定待搜索的文件名
+set "FileName=ui_main.py"
+set "FoundPath="
+
+REM 在当前目录及其子目录中搜索文件
+for /r %%i in (%FileName%) do (
+    set "FoundPath=%%i"
+    goto :FoundFile
+)
+
+:NotFound
+echo not foud ui_main.py: %FileName%, not execute any command!
+goto :EOF
+
+:FoundFile
+echo find path dir: !FoundPath!.
+python !FoundPath!
 echo programmer already exited!
+goto :EOF
+
+endlocal
--- src/run/sh/run.sh
+++ src/run/sh/run.sh
@@ -1,2 +1,16 @@
-python3 ui_main.py
+#!/bin/bash
+
+# 指定待搜索的文件名
+filename="ui_main.py"
+
+# 在当前目录及其子目录中搜索文件
+found_path=$(find . -name "$filename" -type f -print -quit)
+
+# 检查是否找到文件
+if [ -n "$found_path" ]; then
+    echo "find file path: $found_path"
+    python3 found_path
+else
+    echo "not found file path: $filename"
+fi
 printf 'programmer already exited!'
