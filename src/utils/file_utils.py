bb7fa94d6b2eb93a8bfcec4b296ff84212905845
caozhaoqi <1150118968@qq.com>
2024-02-29 18:30:41 +0800

    update code fix bugs for path

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -264,7 +264,19 @@ def find_img_result(path):
     """
     folder_name = None
     # 分割路径到各个部分
-    parts = path.split("/")
+    os_name = sys.platform
+    parts = None
+    if os_name == 'win32':
+        # Windows系统
+        parts = path.split("\\")
+
+    elif os_name == 'darwin':
+        # macOS系统
+        parts = path.split("/")
+
+    elif os_name.startswith('linux'):
+        # Linux系统
+        parts = path.split("/")
 
     # 从后往前遍历，找到第一个包含'_img_result'的文件夹名
     for i in range(len(parts) - 1, -1, -1):
