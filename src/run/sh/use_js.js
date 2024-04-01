e7932c89ea15905fd35d8b170ba5c0b8378e4de2
caozhaoqi <1150118968@qq.com>
2024-04-01 15:28:11 +0800

    update about and add sh, keyword

---
+++ src/run/sh/use_js.js
@@ -0,0 +1,18 @@
+//
+// use js at present
+//
+
+function click_all(){
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      if (button.textContent.includes('查看全部') || button.textContent.includes('阅读作品')) {
+        return button;
+      }
+    }
+}
+
+export click_all;
+
+// get page height
+return document.body.scrollHeight
--- src/ui_event/about_dialog_ui.py
+++ src/ui_event/about_dialog_ui.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-import PyQt5.Qt
-from PyQt5.QtCore import QtInfoMsg
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from loguru import logger
@@ -26,7 +23,7 @@ class InformationDialog(QDialog):
         desc_label = QLabel('软件介绍(software desc):')
         layout.addWidget(desc_label)
 
-        desc = "SIS is designed as a tool to crawl target websites."  # 替换为实际的软件版本
+        desc = "SIS is designed as a tool to crawl target websites."
         desc_label_2 = QLabel(desc)
         layout.addWidget(desc_label_2)
 
@@ -40,7 +37,7 @@ class InformationDialog(QDialog):
         author_label = QLabel('作者(author):')
         layout.addWidget(author_label)
 
-        author = 'Copyright © 2023 - 2024 Zhaoqi.Cao | Powered by PyQt5(v5.15.10)'  # 替换为实际的作者姓名
+        author = 'Copyright © 2023 - 2024 Zhaoqi.Cao | Powered by PyQt5(v5.15.10)'
         author_label_2 = QLabel(author)
         layout.addWidget(author_label_2)
 
@@ -53,22 +50,22 @@ class InformationDialog(QDialog):
         website_label = QLabel('访问网站(visit website):')
         layout.addWidget(website_label)
 
-        website = 'https://caozhaoqi.github.io'  # 替换为实际的网站地址
+        website = 'https://caozhaoqi.github.io'
         website_label_2 = QLabel(website)
         layout.addWidget(website_label_2)
 
         modified_label = QLabel('版本修改时间(CST(Shanghai, China)):')
         layout.addWidget(modified_label)
 
-        modified_time = build_date  # 替换为实际的修改时间，根据实际需要更新
+        modified_time = build_date
         modified_label_2 = QLabel(modified_time)
         layout.addWidget(modified_label_2)
 
-        close_button = QPushButton('关闭(close)')  # 添加关闭按钮
-        close_button.clicked.connect(self.close)  # 连接关闭信号和关闭方法
-        layout.addWidget(close_button)  # 将按钮添加到布局中
+        close_button = QPushButton('关闭(close)')
+        close_button.clicked.connect(self.close)
+        layout.addWidget(close_button)
 
-        self.setLayout(layout)  # 设置对话框的布局为刚才创建的布局对象
+        self.setLayout(layout)
 
     def closeEvent(self, event):
         """
@@ -76,8 +73,6 @@ class InformationDialog(QDialog):
         :param event:
         :return:
         """
-        # 在这里你可以添加任何你需要在对话框关闭时执行的代码
         logger.debug('about_message_lookup_visible Dialog is closing!')
         constants.about_message_lookup_visible = False
-        # 调用基类的 closeEvent 方法以确保对话框正常关闭
         super(InformationDialog, self).closeEvent(event)
