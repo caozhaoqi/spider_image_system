574543c4414e3ae479e93c2d9be11a811605e9f4
caozhaoqi <1150118968@qq.com>
2024-04-15 18:32:40 +0800

    update nuitka sh

--- src/run/sh/run_release.sh
+++ src/run/sh/run_release.sh
@@ -3,4 +3,4 @@ pip install Nuitka
 
 cd ../../
 ls -l
-python -m nuitka --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py
+python -m nuitka --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,view --output-dir=out --output=sis-v1.1.2.exe --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py
