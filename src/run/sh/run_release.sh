1f2d810e3cc0e51518b33692e75028d7bec326e3
caozhaoqi <1150118968@qq.com>
2024-05-11 15:18:36 +0800

    add --windows-disable-console package sh

--- src/run/sh/run_release.sh
+++ src/run/sh/run_release.sh
@@ -4,3 +4,5 @@ pip install Nuitka
 cd ../../
 ls -l
 python -m nuitka --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py
+# 不输出控制台
+python -m nuitka --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py --windows-disable-console
