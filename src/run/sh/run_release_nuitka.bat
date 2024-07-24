d5f634d23690eb369dba83a3c8b91c05a71de94e
caozhaoqi <1150118968@qq.com>
2024-07-24 13:42:13 +0800

    update nuitka package sh

--- src/run/sh/run_release_nuitka.bat
+++ src/run/sh/run_release_nuitka.bat
@@ -7,7 +7,7 @@ dir
 copy run\favicon.ico out\ui_main.dist\
 @REM # 不输出控制台 打包前手动复制favicon.ico到out中
 python -m nuitka  --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,jmcomic,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py --windows-disable-console
-@REM 单个文件
-python -m nuitka --onefile --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,jmcomic,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py --windows-disable-console
+@REM 单个文件 windows platform use
+python -m nuitka --onefile --mingw64 --standalone --follow-import-to=config,file,http_tools,image,log,model,router,run,ui_event,utils,jmcomic,view --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/ui_main.py --windows-disable-console --output-filename=SIS_v1.1.9.240724
 @REM python -m nuitka --onefile --mingw64 --standalone --follow-import-to=log,run,utils --output-dir=out --plugin-enable=pyqt5 --windows-icon-from-ico=./run/favicon.ico --show-progress ./run/main_ui.py --windows-disable-console  --output-filename=HMAC_calculator_v1.0.3
 @REM pass
