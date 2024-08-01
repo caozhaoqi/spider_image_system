a843d6c2406002a1a682a43ef63747c7f2848512
czq <1150118968@qq.com>
2024-08-01 02:54:00 +0000

    update filter already upload file
    Signed-off-by: czq <1150118968@qq.com>

--- src/utils/go_file_utils.py
+++ src/utils/go_file_utils.py
@@ -1,10 +1,15 @@
-# import this
-import fnmatch
+"""
+go file upload to server
+file: go_file_utils.py
+author: zq.c
+modify time: 2024/8/1 12:00
+create time: 2024/7/24 12:00
+"""
+
 import json
 import os
 
 import requests
-# from gofilepy import GofileClient
 from loguru import logger
 
 """
@@ -86,29 +91,89 @@ def scan_files_go(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_name_path = os.path.join(root, file)
-            if os.path.getsize(file_name_path) > 0:
-            	file_list.append(os.path.join(root, file))
+            if os.stat(file_name_path).st_size > 0:
+                file_list.append(os.path.join(root, file))
     return file_list
 
 
 @logger.catch
-def upload_all_gofile(path):
+def save_upload_already(txt_data_path, file_path):
     """
 
-    :param path:
+    :param txt_data_path:
+    :param file_path:
+    :return:
+    """
+    # file_name = "go_file_img_upload.txt"
+    file_name = os.path.join(txt_data_path, "go_file_img_upload.txt")
+    with open(file_name, 'a', encoding='utf-8', errors='replace') as f:
+        f.write(file_path + "\n")
+    logger.success(f"save {file_path} finished, will write txt.")
+    return True
+
+
+@logger.catch
+def exists_upload_gofile(txt_path):
+    """
+
+    :param txt_path:
+    :return:
+    """
+
+    file_name = 'go_file_img_upload.txt'
+    full_file_path = os.path.join(txt_path, file_name)
+    if not os.path.exists(full_file_path):
+        # 如果文件不存在，创建它
+        with open(full_file_path, 'w', encoding='utf-8', errors='replace') as f:
+            logger.warning(f"Current {full_file_path} not exists, will create demo txt!")
+    try:
+        with open(full_file_path, 'r', encoding='utf-8', errors='replace') as f:
+            exists_go_file_img = f.readlines()
+        return exists_go_file_img
+    except Exception as e:
+        logger.error(f"Unknown error, detail {e}")
+        return []
+
+
+@logger.catch
+def exists_gofile_img(file_name, txt_list):
+    """
+
+    :param file_name:
+    :param txt_list:
+    :return:
+    """
+    file_name = os.path.basename(file_name)
+    for txt in txt_list:
+        if file_name in txt:
+            return True
+    return False
+
+
+@logger.catch
+def upload_all_gofile(data_path):
+    """
+
+    :param data_path:
     :return:
     """
     try:
-        list_file = scan_files_go(path)
+        list_img_txt = exists_upload_gofile(data_path)
+        list_file = scan_files_go(data_path)
         if not list_file:
-            logger.warning(f"Current path: {path} no files, detail: {list_file}")
+            logger.warning(f"Current path: {data_path} no files, detail: {list_file}")
             return False
         # 使用示例
         for file in list_file:
-            http_upload(file)
+            if not exists_gofile_img(file, list_img_txt):
+                http_upload(file)
+                save_upload_already(data_path, file)
+            else:
+                logger.warning(f"File: {file} Already upload gofile server, skip upload.")
     except Exception as e:
         logger.error(f"ERROR!!! detail: {e}")
 
 
 if __name__ == '__main__':
-    upload_all_gofile('./')
+    path = r'./'
+    upload_all_gofile(path)
