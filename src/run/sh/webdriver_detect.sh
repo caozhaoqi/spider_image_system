273d8eb39902b5925068b68d275c58a3e026ccea
czq <1150118968@qq.com>
2024-08-06 06:54:53 +0000

    update code and doc
    Signed-off-by: czq <1150118968@qq.com>

---
+++ src/run/sh/webdriver_detect.sh
@@ -0,0 +1,4 @@
+# ubuntu detect chrome webdriver .
+echo 'output chrome run result.'
+/home/czq/Desktop/sis/spider_image_system/venv/lib/python3.10/site-packages/selenium/webdriver/common/linux/selenium-manager --browser chrome --debug
+echo 'run finished.'
