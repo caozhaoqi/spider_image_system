61a77b0f3f051ff744d56da771669e5aab2dc424
czq <1150118968@qq.com>
2024-08-07 03:29:19 +0000

    update sh webdriver
    Signed-off-by: czq <1150118968@qq.com>

--- src/run/sh/webdriver_detect.sh
+++ src/run/sh/webdriver_detect.sh
@@ -1,4 +1,16 @@
-# ubuntu detect chrome webdriver .
-echo 'output chrome run result.'
-/home/czq/Desktop/sis/spider_image_system/venv/lib/python3.10/site-packages/selenium/webdriver/common/linux/selenium-manager --browser chrome --debug
-echo 'run finished.'
+filename="selenium-manager"
+
+# 在当前目录及其子目录中搜索文件
+found_path=$(find . -name "$filename" -type f -print -quit)
+
+# 检查是否找到文件
+if [ -n "$found_path" ]; then
+    echo "find file path: $found_path"
+    ./"$found_path" --browser chrome --debug
+else
+    echo "Error! not found file path: $filename"
+fi
+printf 'program already exited!'
+
+# remove unuse /r command
+# sed -i 's/\r//' your_script.sh
